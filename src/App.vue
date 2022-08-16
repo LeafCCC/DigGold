514d22209bf80a6ca8126e04fd7d8aeb09fd894a
LeafCCC <sjtu.zcc@outlook.com>
2022-08-17 01:04:52 +0800

    init the project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { createApp } from 'vue';
+import { NavBar } from 'vant';
+
+const app = createApp();
+app.use(NavBar);
+</script>
+
+<template>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <van-nav-bar title="标题" />
+</head>
+  
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,6 @@
+
+<template>
+  HelloWorld
+</template>
+
+
