ab776b899a657d097e46b2df596a0e658a6115f5
LeafCCC <sjtu.zcc@outlook.com>
2022-08-17 19:36:30 +0800

    初版的菜单及页面完成

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -2,36 +2,43 @@
   <el-menu
     :default-active="activeIndex"
     class="el-menu-demo"
-    mode="horizontal"
+    mode="horizontal"  
+    :ellipsis="false"
     @select="handleSelect"
   >
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
+    
+    <el-menu-item index="0"><img src="../assets/DigGoldHome.svg"></el-menu-item>
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-menu-item index="2">沸点</el-menu-item>
+    <el-menu-item index="3">课程</el-menu-item>
+    <el-menu-item index="4">直播</el-menu-item>
+    <el-menu-item index="5">活动</el-menu-item>
+    <el-menu-item index="6">商城</el-menu-item>
+    <el-menu-item index="7">app</el-menu-item>
+    <el-menu-item index="8">插件</el-menu-item>
+    <div class="flex-grow" />
+    <el-sub-menu index="9">
+      <template #title>创作者中心</template>
+      <el-menu-item index="9-1">写文章</el-menu-item>
+      <el-menu-item index="9-2">发沸点</el-menu-item>
+      <el-menu-item index="9-3">写代码</el-menu-item>
     </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item index="10">会员</el-menu-item>
+    <el-menu-item index="11">登录</el-menu-item>
   </el-menu>
-  <div class="h-6" />
-
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
 const activeIndex = ref('1')
-const activeIndex2 = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
 </script>
 
+<style>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
