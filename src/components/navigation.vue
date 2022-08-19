2f4444c36eb37a1288c1e15f6407ca4ee32f0c45
yangguang <1124798482@qq.com>
2022-08-19 23:15:52 +0800

    导航栏左右分离

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,38 +1,51 @@
 <template>
   <div class="nav">
-    <div class="nav_logo">
-      <router-link to="/">
-        <img class="logo-image" alt="稀土堀金" src="../assets/iconwithtext.svg">
-      </router-link>
+    <div class="nav_left">
+      <div class="nav_logo">
+        <router-link to="/">
+          <img class="logo-image" alt="稀土堀金" src="../assets/iconwithtext.svg">
+        </router-link>
+      </div>
+      <div class="left_menu">
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu"
+          mode="horizontal"  
+          :ellipsis="false"
+          @select="handleSelect"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">沸点</el-menu-item>
+          <el-menu-item index="3">课程</el-menu-item>
+          <el-menu-item index="4">直播</el-menu-item>
+          <el-menu-item index="5">活动</el-menu-item>
+          <el-menu-item index="6">商城</el-menu-item>
+          <el-menu-item index="7">app</el-menu-item>
+          <el-menu-item index="8">插件</el-menu-item>
+        </el-menu>
+      </div>
     </div>
-    <div class="nav_menu">
-
-      <el-menu
-        :default-active="activeIndex"
-        class="el-menu"
-        mode="horizontal"  
-        :ellipsis="false"
-        @select="handleSelect"
-      >
-        <el-menu-item index="1">首页</el-menu-item>
-        <el-menu-item index="2">沸点</el-menu-item>
-        <el-menu-item index="3">课程</el-menu-item>
-        <el-menu-item index="4">直播</el-menu-item>
-        <el-menu-item index="5">活动</el-menu-item>
-        <el-menu-item index="6">商城</el-menu-item>
-        <el-menu-item index="7">app</el-menu-item>
-        <el-menu-item index="8">插件</el-menu-item>
-        <div class="flex-grow" />
-        <el-sub-menu index="9">
-          <template #title>创作者中心</template>
-          <el-menu-item index="9-1">写文章</el-menu-item>
-          <el-menu-item index="9-2">发沸点</el-menu-item>
-          <el-menu-item index="9-3">写代码</el-menu-item>
-        </el-sub-menu>
-        <el-menu-item index="10">会员</el-menu-item>
-        <el-menu-item index="11">登录</el-menu-item>
-      </el-menu>
+    <div class="nav-right">
+      <div class="right_menu">
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu"
+          mode="horizontal"  
+          :ellipsis="false"
+          @select="handleSelect"
+        >
+          <el-sub-menu index="9">
+            <template #title>创作者中心</template>
+            <el-menu-item index="9-1">写文章</el-menu-item>
+            <el-menu-item index="9-2">发沸点</el-menu-item>
+            <el-menu-item index="9-3">写代码</el-menu-item>
+          </el-sub-menu>
+          <el-menu-item index="10">会员</el-menu-item>
+          <el-menu-item index="11">登录</el-menu-item>
+        </el-menu>
+      </div>
     </div>
+        
   </div>  
   
 </template>
@@ -56,13 +69,13 @@ const handleSelect = (key: string, keyPath: string[]) => {
   background-color: #fff;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.12);
 }
-.nav_logo {
+.nav_left {
   display: flex;
+  justify-content: flex-start;
   align-items: center;
 }
-.nav_menu {
+.nav_logo {
   display: flex;
-  justify-content: flex-end;
   align-items: center;
 }
 .logo-image {
@@ -70,7 +83,5 @@ const handleSelect = (key: string, keyPath: string[]) => {
   width: 107px;
   height: 22px;
 }
-.flex-grow {
-  flex-grow: 1;
-}
+
 </style>
