24df0d6e685ed3fdb1797e338358390b0b045345
yangguang <1124798482@qq.com>
2022-08-24 18:37:15 +0800

    文章详情页完善，增加左侧按钮。

---
+++ src/components/ArticleLeftBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="icons">
+      <el-button :icon="Star" size="large" circle />
+    </div>
+    <div class="icons">
+      <el-button :icon="Edit" size="large" circle />
+    </div>
+    <div class="icons">
+      <el-button :icon="Share" size="large" circle />
+    </div>
+    <div class="icons">
+      <el-button :icon="Opportunity" size="large" circle />
+    </div>
+    <div class="icons">
+      <el-button :icon="Printer" size="large" circle />
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {
+  Star,
+  Edit,
+  Share,
+  Opportunity,
+  Printer,
+} from '@element-plus/icons-vue'
+
+// export default {
+//   name: "ArticleLeftBar"
+// }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+  .icons {
+    margin:10px 5px 15px 20px;
+  }
+
+</style>
--- src/views/ArticleDemo.vue
+++ src/views/ArticleDemo.vue
@@ -2,6 +2,7 @@
     import BasicLayout from "@/layouts/default/index.vue"
     import ArticleContent from '../components/ArticleContent.vue';
     import ArticleMenu from '../components/ArticleMenu.vue';
+    import ArticleLeftBar from "@/components/ArticleLeftBar.vue";
 
     const article = {
       title: '文章标题',
@@ -17,10 +18,13 @@
 </script>
 <template>
   <BasicLayout></BasicLayout>
-  <div class="main">
-    <div class="main-inside">
-      <div class="main-inside-article">
-        <div class="main-inside-article-info">
+  <div class="container">
+    <div class="container-inside">
+      <div class="container-inside-leftBar">
+        <ArticleLeftBar></ArticleLeftBar>
+      </div>
+      <div class="container-inside-article">
+        <div class="container-inside-article-info">
           <div class="title">
             <h1>{{ article.title }}</h1>
           </div>
@@ -47,15 +51,17 @@
 
         </div>
 
-        <div class="main-inside-article-markdown">
+        <div class="container-inside-article-markdown">
           <ArticleContent></ArticleContent>
         </div>
       </div>
-      <div class="main-inside-leftBar">
-
-      </div>
-      <div class="main-inside-rightBar">
-
+      <div class="container-inside-rightBar">
+<!--        <div>-->
+<!--          <p>目录</p>-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <ArticleMenu></ArticleMenu>-->
+<!--        </div>-->
       </div>
     </div>
   </div>
@@ -63,73 +69,64 @@
 </template>
 
 <style lang="scss" scoped>
-.main {
+.container {
   width: 100%;
   display: flex;
   justify-content: center;
-  .main-inside {
+  .container-inside {
     margin-top: 16px;
     width: 100%;
     position: relative;
     display: flex;
     justify-content: space-between;
-  }
-  .main-inside-article {
-    width: 70%;
-    background: white;
-    border-radius: 2px;
-    padding: 24px;
-
-    .main-inside-article-info {
-      display: flex;
-      flex-direction: column;
-
-      .title {
-
-      }
-      .author {
+    .container-inside-article {
+      width: 60%;
+      background: white;
+      border-radius: 2px;
+      padding: 24px;
+      .container-inside-article-info {
         display: flex;
-        justify-content: space-between;
-        .author-left {
+        flex-direction: column;
+        .author {
           display: flex;
-          flex-direction: row;
-          justify-content: flex-start;
-          span {
-            display: block;
+          justify-content: space-between;
+          .author-left {
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+            span {
+              display: block;
+            }
+            .author-name {
+              color: #515767;
+              font-size: 16px;
+            }
+            .create-time {
+              color: #8A919F;
+              font-size: 14px;
+              margin-top: 7px;
+            }
           }
-          .author-name {
-            color: #515767;
-            font-size: 16px;
-          }
-          .create-time {
-            color: #8A919F;
-            font-size: 14px;
-            margin-top: 7px;
-          }
-        }
 
-        .button {
-          margin-top: 100px;
+          .button {
+            margin-top: 100px;
+          }
         }
       }
-
-
-
+      .container-inside-article-markdown {
+      }
     }
-    .main-inside-article-markdown {
-
+    .container-inside-leftBar {
+      margin-top: 2rem;
+      margin-left: 2rem;
+      width: 10%;
+    }
+    .container-inside-rightBar {
+      width: 20%;
+      border-radius: 2px;
+      background: beige;
     }
   }
-  .main-inside-leftBar {
-    position: fixed;
-    top: 200px;
-    width: 5%;
-    margin-left: -5rem;
-  }
-  .main-inside-rightBar {
-    width: 20%;
-    border-radius: 2px;
-    background: beige;
-  }
+
 }
 </style>
