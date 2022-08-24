0cd1e6fc72f9f71d446666bb7031638b8cc3ddbc
degulabo <1842298680@qq.com>
2022-08-24 22:41:17 +0800

    chore(project): import axios

---
+++ src/configs/host.js
@@ -0,0 +1,14 @@
+export default {
+  mock: {
+    API: 'http://127.0.0.1:4523/m1/1479235-0-default'
+  },
+  test: {
+    API: 'https://api.juejin.cn'
+  },
+  development: {
+    API: 'https://api.juejin.cn'
+  },
+  production: {
+    API: 'https://api.juejin.cn'
+  }
+}
